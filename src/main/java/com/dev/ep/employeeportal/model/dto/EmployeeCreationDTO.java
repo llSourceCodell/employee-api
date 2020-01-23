06d992990fa01a5357d26a94fcb2a3e4650f32aa
Sreejith S <sreejithsev@gmail.com>
2020-01-23 22:28:55 +0530

    Add DTO classes

---
+++ src/main/java/com/dev/ep/employeeportal/model/dto/EmployeeCreationDTO.java
@@ -0,0 +1,51 @@
+package com.dev.ep.employeeportal.model.dto;
+
+import com.dev.ep.employeeportal.model.Designation;
+import com.dev.ep.employeeportal.model.Location;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.time.LocalDateTime;
+
+public class EmployeeCreationDTO {
+
+    private String firstName;
+    private String lastName;
+    private Designation designation;
+    private Location location;
+    @JsonIgnore
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @JsonIgnore
+    private LocalDateTime editedAt = LocalDateTime.now();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Designation getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(Designation designation) {
+        this.designation = designation;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
